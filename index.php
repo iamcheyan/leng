b7cc8cceeee1347a3de743a2125438a9a72cc654
iamcheyan <iamcheyan@163.com>
2012-06-26 15:27:04 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>leng</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="js/jq.js" type="text/javascript"></script>
+<script src="js/l.js" type="text/javascript"></script>
+<script src="js/jquery.easing.1.3.js" type="text/javascript"></script>
+<script src="js/jquery.vgrid.0.1.2-mod.js" type="text/javascript"></script>
+<script src="js/jquery.lightbox.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(function() {
+	var vg = $(".sl-grid-wrapper").vgrid({
+		easeing: "easeOutQuint",
+		time: 400,
+		delay: 40,
+		selFitWidth: ".sl-grid-wrapper",
+		selRefGrid: ".sl-grid-box",
+		forceAnim: 0
+	});
+	$(".sl-grid-wrapper li a").lightbox();
+});
+</script>
+<!--[if lt IE 9]>
+<script src="js/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+
+<leng class="fn-clear">
+	<header class="fn-left">
+		<h1>leng.冷</h1>
+		<nav>
+			<ul>
+				<li>photos</li>
+				<li>tags</li>
+				<li>article</li>
+			</ul>
+		</nav>
+	</header>
+	<section class="content">
+		<ul class="sl-grid-wrapper con-list">
+			<li class="sl-grid-box" style="position: absolute; left: 232px; top: 0px; "><a href="images/photo/02.jpg"><img src="images/thumbnail/02.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 464px; top: 0px; "><a href="images/photo/03.jpg"><img src="images/thumbnail/03.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 696px; top: 0px; "><a href="images/photo/04.jpg"><img src="images/thumbnail/04.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 928px; top: 0px; "><a href="images/photo/05.jpg"><img src="images/thumbnail/05.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 232px; top: 185px; "><a href="images/photo/06.jpg"><img src="images/thumbnail/06.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 928px; top: 263px; "><a href="images/photo/07.jpg"><img src="images/thumbnail/07.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 464px; top: 298px; "><a href="images/photo/08.jpg"><img src="images/thumbnail/08.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 0px; top: 314px; "><a href="images/photo/09.jpg"><img src="images/thumbnail/09.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 696px; top: 343px; "><a href="images/photo/10.jpg"><img src="images/thumbnail/10.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 232px; top: 420px; "><a href="images/photo/11.jpg"><img src="images/thumbnail/11.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 928px; top: 424px; "><a href="images/photo/12.jpg"><img src="images/thumbnail/12.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 464px; top: 468px; "><a href="images/photo/01.jpg"><img src="images/thumbnail/01.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 0px; top: 549px; "><a href="images/photo/02.jpg"><img src="images/thumbnail/02.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 696px; top: 578px; "><a href="images/photo/03.jpg"><img src="images/thumbnail/03.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 232px; top: 655px; "><a href="images/photo/04.jpg"><img src="images/thumbnail/04.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 928px; top: 715px; "><a href="images/photo/05.jpg"><img src="images/thumbnail/05.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 0px; top: 734px; "><a href="images/photo/06.jpg"><img src="images/thumbnail/06.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 464px; top: 782px; "><a href="images/photo/07.jpg"><img src="images/thumbnail/07.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 696px; top: 876px; "><a href="images/photo/08.jpg"><img src="images/thumbnail/08.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 464px; top: 943px; "><a href="images/photo/09.jpg"><img src="images/thumbnail/09.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 0px; top: 969px; "><a href="images/photo/10.jpg"><img src="images/thumbnail/10.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 928px; top: 978px; "><a href="images/photo/11.jpg"><img src="images/thumbnail/11.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 232px; top: 998px; "><a href="images/photo/12.jpg"><img src="images/thumbnail/12.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 696px; top: 1046px; "><a href="images/photo/01.jpg"><img src="images/thumbnail/01.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 464px; top: 1178px; "><a href="images/photo/02.jpg"><img src="images/thumbnail/02.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 0px; top: 1204px; "><a href="images/photo/03.jpg"><img src="images/thumbnail/03.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 928px; top: 1213px; "><a href="images/photo/04.jpg"><img src="images/thumbnail/04.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 232px; top: 1289px; "><a href="images/photo/05.jpg"><img src="images/thumbnail/05.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 696px; top: 1360px; "><a href="images/photo/06.jpg"><img src="images/thumbnail/06.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 464px; top: 1363px; "><a href="images/photo/07.jpg"><img src="images/thumbnail/07.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 0px; top: 1502px; "><a href="images/photo/08.jpg"><img src="images/thumbnail/08.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 464px; top: 1524px; "><a href="images/photo/09.jpg"><img src="images/thumbnail/09.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 232px; top: 1552px; "><a href="images/photo/10.jpg"><img src="images/thumbnail/10.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 928px; top: 1556px; "><a href="images/photo/11.jpg"><img src="images/thumbnail/11.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 696px; top: 1595px; "><a href="images/photo/12.jpg"><img src="images/thumbnail/12.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 0px; top: 1672px; "><a href="images/photo/01.jpg"><img src="images/thumbnail/01.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 464px; top: 1759px; "><a href="images/photo/02.jpg"><img src="images/thumbnail/02.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 232px; top: 1787px; "><a href="images/photo/03.jpg"><img src="images/thumbnail/03.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 928px; top: 1791px; "><a href="images/photo/04.jpg"><img src="images/thumbnail/04.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 696px; top: 1886px; "><a href="images/photo/05.jpg"><img src="images/thumbnail/05.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 464px; top: 1944px; "><a href="images/photo/06.jpg"><img src="images/thumbnail/06.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 0px; top: 1986px; "><a href="images/photo/07.jpg"><img src="images/thumbnail/07.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 232px; top: 2085px; "><a href="images/photo/08.jpg"><img src="images/thumbnail/08.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 928px; top: 2134px; "><a href="images/photo/09.jpg"><img src="images/thumbnail/09.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 0px; top: 2147px; "><a href="images/photo/10.jpg"><img src="images/thumbnail/10.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 696px; top: 2149px; "><a href="images/photo/11.jpg"><img src="images/thumbnail/11.jpg"></a></li>
+			<li class="sl-grid-box" style="position: absolute; left: 464px; top: 2179px; "><a href="images/photo/12.jpg"><img src="images/thumbnail/12.jpg"></a></li>
+		</ul>
+	</section>
+</leng>
+</body>
+</html>
---
+++ js/l.js
@@ -0,0 +1,18 @@
+﻿/*var countWidth = function(){*/
+	//$sectionWidth = $("section.content").width();
+	//$conListWidth = $(".con-list").width();
+	//$padLeft = $sectionWidth - $conListWidth;
+	//console.log($padLeft)
+	//$("header").css({
+		//"padding-left" :$padLeft 
+	//})
+//};
+
+//$(window).load(function(){
+	//countWidth();
+	//window.onresize = countWidth;
+//});
+
+//$(function(){
+	
+/*});*/
